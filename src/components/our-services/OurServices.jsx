f8959aad90c74e43ed97b605cfbf9df5d5341d2b
Carlos Cataño <serdnah2@gmail.com>
2022-10-12 10:01:08 -0500

    Creating our-services component

---
+++ src/components/our-services/OurServices.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import styles from "@styles/components/our-services/OurServices.module.scss";
+
+export default function OurServices({ ourServices }) {
+  return (
+    <div  className={styles.ourServicesWrapper}>
+      <div className={styles.ourServicesHeading}>Nuestros servicios</div>
+      <div className={styles.ourServicesListWrapper}>
+        {ourServices.map((service, index) => (
+          <div className="our-services-list-item" key={index}>
+            <div className="our-services-list-item-icon">
+                <Image
+                  src={service.img}
+                  alt="Service icon"
+                  width="50"
+                  height="50">
+                </Image>
+            </div>
+            <div className={styles.ourServicesListItemHeading}>
+                {service.heading}
+            </div>
+            <div className={styles.ourServicesListItemCopy}>
+                {service.copy}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
