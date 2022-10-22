1f4d42de48e2680bfd80b44e029f060d306f1ff2
Carlos Cataño <serdnah2@gmail.com>
2022-10-22 13:15:38 -0500

    Fixing layout for mobile

--- src/components/information/Information.jsx
+++ src/components/information/Information.jsx
@@ -5,8 +5,8 @@ import styles from "@styles/components/information/Information.module.scss";
 
 export default function Information({ data }) {
   return (
-    <div className={`${styles.informationWrapper} ${data.layout == "reverse" ? styles.informationWrapperReverse : ""}`}>
-      <div className={`${data.isMainInformation ? styles.isMainInformation : ""}`}>
+    <div className={`${styles.informationWrapper} ${data.layout == "reverse" && styles.informationWrapperReverse}`}>
+      <div className={`${data.isMainInformation && styles.isMainInformation}`}>
         <h2 className={styles.informationTitle}>{data.title}</h2>
         <p className={styles.informationCopy}>{data.copy}</p>
         {data.link && (
@@ -17,7 +17,19 @@ export default function Information({ data }) {
           </Link>
         )}
       </div>
-      <div>
+      <div className={`${styles.informationWrapperImage} only-mobile`}>
+        <Image
+          className={styles.informationImage}
+          src="/img/home/results-online.png"
+          alt={data.img.alt}
+          width="100%"
+          height="186px"
+          layout="responsive"
+          objectFit="contain"
+        >
+        </Image>
+      </div>
+      <div className="only-desktop">
         <Image
           src={data.img.src}
           alt={data.img.alt}
