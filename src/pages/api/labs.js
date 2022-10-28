4b37afe403774a0b4bf252a719125af839c291ca
Mateo Castaño <matew-17@hotmail.com>
2022-10-27 23:02:07 -0500

    [Ship] Improving API and allowing code search

--- src/pages/api/labs.js
+++ src/pages/api/labs.js
@@ -4,13 +4,23 @@ export default async function handler(req, res) {
   try {
     const { search, limit = 0 } = req?.query;
 
+    if (!Boolean(search)) {
+      return res.status(400).json({
+        error: "Error getting data",
+        cause: "Search value cannot be empty",
+      });
+    }
+
     const client = await clientPromise;
     const db = client.db("cic");
 
     const labs = await db
       .collection("labs")
       .find({
-        nombre_prueba: { $regex: search, $options: "i" },
+        $or: [
+          { nombre_prueba: { $regex: search, $options: "i" } },
+          { codigo_cups: { $regex: search, $options: "i" } },
+        ],
       })
       .sort({ nombre_prueba: -1 })
       .limit(Number(limit))
@@ -18,6 +28,8 @@ export default async function handler(req, res) {
 
     res.json(labs);
   } catch (e) {
-    console.error(e);
+    res
+      .status(500)
+      .json({ error: "Error getting data from server", cause: e.name });
   }
 }
