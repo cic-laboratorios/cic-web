22419463535695cc1d1db7bac6f20a94cdfeb930
Andrés Cataño <serdnah2@gmail.com>
2022-10-31 08:46:09 -0500

    Adding scroll behavior to hide and show (#33)

---
+++ src/utils/NavScroll.js
@@ -0,0 +1,29 @@
+const { useState, useEffect } = require("react");
+
+const useScrollDirection = () => {
+  const [scrollDirection, setScrollDirection] = useState(null);
+
+  useEffect(() => {
+    let lastScrollY = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+      const direction = scrollY > lastScrollY ? "down" : "up";
+      if (
+        direction !== scrollDirection &&
+        (scrollY - lastScrollY > 5 || scrollY - lastScrollY < -5)
+      ) {
+        setScrollDirection(direction);
+      }
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+    };
+    window.addEventListener("scroll", updateScrollDirection); // add event listener
+    return () => {
+      window.removeEventListener("scroll", updateScrollDirection); // clean up
+    };
+  }, [scrollDirection]);
+
+  return scrollDirection;
+};
+
+export default useScrollDirection;
