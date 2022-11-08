38b66212d53f2ef3c06dfa1c09e6ff878ce9e235
Carlos Cataño <serdnah2@gmail.com>
2022-11-08 08:45:44 -0500

    Adding scroll behavior for our test page

--- src/components/layouts/MainLayout.jsx
+++ src/components/layouts/MainLayout.jsx
@@ -5,13 +5,18 @@ import Footer from "@components/footer/Footer";
 
 import styles from "@styles/layouts/MainLayout.module.scss";
 
-export default function MainLayout({ navLinks, mobileNavLinks, children, isFixedHeaderAndFooter }) {
+export default function MainLayout({
+  navLinks,
+  mobileNavLinks,
+  children,
+  isOurTestPage,
+}) {
   return (
-    <div className={`${isFixedHeaderAndFooter && styles.fixedHeaderAndFooter}`}>
+    <div className={`${isOurTestPage && styles.isOurTestPage}`}>
       <Head>
         <title>CIC - Web</title>
       </Head>
-      <Nav navLinks={navLinks} mobileNavLinks={mobileNavLinks} isFixedHeaderAndFooter={isFixedHeaderAndFooter}></Nav>
+      <Nav navLinks={navLinks} mobileNavLinks={mobileNavLinks}></Nav>
       <main className={styles.mainWrapper}>{children}</main>
       <Footer></Footer>
     </div>
