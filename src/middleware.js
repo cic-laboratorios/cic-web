fa81df9b496f388774c1d1671db34d492c70c9d0
Mateo Castaño <matew-17@hotmail.com>
2022-12-21 19:03:15 -0500

    Removing console.log

--- src/middleware.js
+++ src/middleware.js
@@ -3,8 +3,6 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const url = request.nextUrl.clone();
 
-  console.log(url);
-
   if (url.pathname === "/webmail") {
     return NextResponse.redirect("https://webmail.ciclaboratorios.com/");
   }
