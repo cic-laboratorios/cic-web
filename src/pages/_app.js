f52033777bc1ce78fab6359ac57196395a81acca
Cuadros <juan.josecuadros1@gmail.com>
2025-08-30 20:42:19 -0500

    added vercel speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 import "../styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
   <>
-  <Analytics />
-  <Component {...pageProps} />
+    <Analytics />
+    <Component {...pageProps} />
+    <SpeedInsights />
   </>
   );
 }
